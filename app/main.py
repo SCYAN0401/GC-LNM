95e868eb7b6a5dc7fca748c25e2385a0afea4e0d
Shangcheng Yan <116451103+SCYAN0401@users.noreply.github.com>
2024-08-25 22:30:40 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -142,16 +142,16 @@ def main():
                 T_category_broad = {'T1a': 'T1', 'T1b': 'T1', 'T2': 'T2', 'T3': 'T3', 'T4a': 'T4', 'T4b': 'T4'}[T_category]
                 
                 ylabels = [
-                    str(Tumor_size) + ' mm' + ' = ' + 'Tumor size',
-                    str(T_category_broad) + ' = ' + 'T category, broad',
-                    str(T_category) + ' = ' + 'T category',
-                    str(SRCC) + ' = ' + 'SRCC', 
-                    str(Grade) + ' = ' + 'Grade',
-                    str(Location_l) + ' = ' + 'Location - Lower', 
-                    str(Location_m) + ' = ' + 'Location - Middle', 
-                    str(Location_u) + ' = ' + 'Location - Upper', 
-                    str(Histology_dt) + ' = ' + 'Histology - Diffuse type', 
-                    str(Histology_it) + ' = ' + 'Histology - Intestinal type',     
+                    Tumor_size + ' mm' + ' = ' + 'Tumor size',
+                    T_category_broad + ' = ' + 'T category, broad',
+                    T_category + ' = ' + 'T category',
+                    SRCC + ' = ' + 'SRCC', 
+                    Grade + ' = ' + 'Grade',
+                    Location_l + ' = ' + 'Location - Lower', 
+                    Location_m + ' = ' + 'Location - Middle', 
+                    Location_u + ' = ' + 'Location - Upper', 
+                    Histology_dt + ' = ' + 'Histology - Diffuse type', 
+                    Histology_it + ' = ' + 'Histology - Intestinal type',     
                 ]
 
                 with col2:
@@ -167,15 +167,15 @@ def main():
                     
                     st.write('SHAP plot')
                     figure = shap.plots.waterfall(explanation, show = False)
-                    # ax_ = figure.get_axes()[0]
+                    ax_ = figure.get_axes()[0]
                     
-                    # tick_labels = ax_.yaxis.get_majorticklabels()
-                    # for i in range(len(sorted_ylabels)):
-                    #     tick_labels[i].set_color("black")
+                    tick_labels = ax_.yaxis.get_majorticklabels()
+                    for i in range(len(sorted_ylabels)):
+                        tick_labels[i].set_color("black")
                         
-                    # ax_.set_yticks(np.arange(len(sorted_ylabels)))
-                    # ax_.set_yticklabels(sorted_ylabels)
-                    # figure = ax_.get_figure()
+                    ax_.set_yticks(np.arange(len(sorted_ylabels)))
+                    ax_.set_yticklabels(sorted_ylabels)
+                    figure = ax_.get_figure()
                     
                     st_shap(figure, width=900, height=600)
 
